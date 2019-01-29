199a8cf8e218df01485b4d2998c7ff7cc8b7f837
pyu3 <pyu3@mail.sfsu.edu>
2019-01-28 16:14:44 -0800

    User can enter a bill amount, choose a tip percentage, and see the tip and total values

--- Tipsy/ViewController.swift
+++ Tipsy/ViewController.swift
@@ -9,12 +9,44 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
+        
+    } // end viewDidLoad function
 
-
-}
+    @IBAction func onEdit(_ sender: Any) {
+        
+        print("Tap Detected")
+        view.endEditing(true)
+        
+    } // end onEdit function
+    
+    @IBAction func calculateTip(_ sender: Any) {
+        
+        // Get the bill amount
+        let bill = Double(billField.text!) ?? 0
+    
+        // Calculate the tip and total
+        let tipPercentages = [0.1, 0.15, 0.2]
+        
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+        
+        // Update the tip and total labels
+        if (bill < 1000000000) {
+            tipLabel.text = String.localizedStringWithFormat("$%.2f", tip)
+            totalLabel.text = String.localizedStringWithFormat("$%.2f", total)
+        }
+        
+    } // end calculateTip function
+    
+} // end ViewController class
 
